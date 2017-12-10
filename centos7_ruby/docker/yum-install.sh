5f07c9d1394646a29323b21458841a35acac6634
Aaron Bronow <aaron@aaronbronow.com>
2017-12-10 16:08:30 -0500

    reduce upgrade steps

--- centos7_ruby/docker/yum-install.sh
+++ centos7_ruby/docker/yum-install.sh
@@ -8,14 +8,9 @@
 # Uncomment if you want to upgrade packages before installing
 # yum -y upgrade
 
-yum -y update
-yum -y upgrade
-yum clean all
 yum -y install epel-release
-#yum -y update
 rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
 yum -y update
-# yum -y install vim git-core postgresql95-server postgresql95-contrib postgresql95-devel man nano
 yum -y install which wget sudo python python-pip git-core ssh-agent postgresql95-contrib postgresql95-devel
 
 # install RVM requirements
@@ -24,8 +19,3 @@ yum -y install patch autoconf automake bison bzip2 gcc-c++ libffi-devel libtool 
 yum -y install firefox              # Required by some Capybara tests.
 yum -y install fontconfig           # Required by PhantomJS.
 yum -y install graphviz             # Required by rails-erd gem.
-
-# /usr/pgsql-9.5/bin/postgresql95-setup initdb
-# systemctl start postgresql-9.5.service
-# systemctl enable postgresql-9.5.service
-# su - postgres -c "psql postgres -tAc \"SELECT 1 FROM pg_roles WHERE rolname='vagrant'\" | grep -q 1 || createuser -s vagrant"
