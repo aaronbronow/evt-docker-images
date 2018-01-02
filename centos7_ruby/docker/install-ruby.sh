0b91e166e23abe923f3577d3141febe28585ba87
Aaron Bronow <aaron@aaronbronow.com>
2018-01-01 20:21:48 -0800

    move ruby setup into a script

--- centos7_ruby/docker/install-ruby.sh
+++ centos7_ruby/docker/install-ruby.sh
@@ -4,6 +4,10 @@ source $HOME/.rvm/scripts/rvm
 
 rvm use --default --install $1
 
+echo PATH=/home/evt/.rvm/gems/ruby-$1/bin:/home/evt/.rvm/gems/ruby-$1@global/bin:/home/evt/.rvm/rubies/ruby-$1/bin:\$PATH >> ~/.bashrc
+echo GEM_HOME=/home/evt/.rvm/gems/ruby-$1 >> ~/.bashrc
+echo GEM_PATH=/home/evt/.rvm/gems/ruby-$1:/home/evt/.rvm/gems/ruby-$1@global >> ~/.bashrc
+
 shift
 
 if (( $# ))
--- centos7_ruby/docker/install-rvm.sh
+++ centos7_ruby/docker/install-rvm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s $1
-echo -e "install: --no-document\nupdate: --no-document" > ~/.gemrc
+echo 'install: --no-document\nupdate: --no-document' > ~/.gemrc
 
 # Install nodejs (needed by coffescripts)
 curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
@@ -11,4 +11,4 @@ nvm install v5.8.0
 pip install awscli==1.11.18 --user
 source ~/.bash_profile
 
-echo -e 'PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+echo 'PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
